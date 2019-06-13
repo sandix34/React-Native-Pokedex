eed4f26de4cc05a682604d3cb0c397fe7550babd
sandrine <sandrinezocca@gmail.com>
2019-06-13 14:00:18 +0200

    écran détails

--- screens/Details.js
+++ screens/Details.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+
+import { capitalize } from '../utils/helpers'
 
 export default class Details extends Component {
+
+  static navigationOptions = ({ navigation }) => {
+    const name = navigation.getParam('name')
+    return {
+      title: capitalize(name) 
+    }
+  }
+
     render() {
+
+      const { navigation } = this.props
+      const name = navigation.getParam('name')
+      const sprites = navigation.getParam('sprites')
+
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
               <Text>
-                  Details screen
+                  {capitalize(name)}
               </Text>
+              <Image
+                style={{
+                  width: 96,
+                  height: 96,
+                }}
+                source={{ uri: sprites.front_default }}
+              />
             </View>
           ); 
     }
