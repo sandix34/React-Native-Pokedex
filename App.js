5dd16945b355d6a45e2f5320543c5bf59083aecd
sandrine <sandrinezocca@gmail.com>
2019-06-13 14:47:38 +0200

    menu navigation en fonction de la plateforme

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import Pokedex from './screens/Pokedex'
+import { Platform } from 'react-native'
 
 import { TabNavigator, DrawerNavigator } from './routes'
 import { createAppContainer } from 'react-navigation'
 
-const Navigator = createAppContainer(DrawerNavigator)
- 
-export default class App extends Component {
-  render () {
-    return (
-      <Navigator />
-    );
-  }
-  
-}
+const IosNavigator = createAppContainer(TabNavigator)
+const AndroidNavigator = createAppContainer(DrawerNavigator)
+
+const App = () => Platform.OS === 'ios' ? <IosNavigator /> : <AndroidNavigator />
+
+export default App
 
